b77f96dc278b40b055b71ede21099e30f9266861
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-02-13 20:21:50 -0500

    html template and mapping, write/copy file, blank css

--- Index.js
+++ Index.js
@@ -4,6 +4,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const generatePage = require("./src/templateHtml");
+const fs = require("fs");
 
 let manager = [];
 let engineer = [];
@@ -24,39 +25,39 @@ function promptUser() {
         name: "name",
         message: "Enter Employee name:",
         validate: (nameInput) => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log("Please enter name!");
-              return false;
-            }
-          },
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter name!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "id",
         message: "Enter employee ID number:",
         validate: (idInput) => {
-            if (idInput) {
-              return true;
-            } else {
-              console.log("Please enter id!");
-              return false;
-            }
-          },
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter id!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "email",
         message: "Enter employee email:",
         validate: (emailInput) => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log("Please enter email address!");
-              return false;
-            }
-          },
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter email address!");
+            return false;
+          }
+        },
       },
     ])
     .then(({ name, id, email, role }) => {
@@ -85,7 +86,7 @@ function promptUser() {
           ])
           .then(({ officeNumber, addNewEmp }) => {
             manager.push(new Manager(name, id, email, officeNumber));
-            console.log(teamArr);
+            //console.log(teamArr);
             if (addNewEmp) {
               return promptUser();
             }
@@ -115,7 +116,7 @@ function promptUser() {
           ])
           .then(({ github, addNewEmp }) => {
             engineer.push(new Engineer(name, id, email, github));
-            console.log(teamArr);
+            //console.log(teamArr);
             if (addNewEmp) {
               return promptUser();
             }
@@ -145,7 +146,7 @@ function promptUser() {
           ])
           .then(({ school, addNewEmp }) => {
             intern.push(new Intern(name, id, email, school));
-            console.log(teamArr);
+            //console.log(teamArr);
             if (addNewEmp) {
               return promptUser();
             }
@@ -154,8 +155,52 @@ function promptUser() {
     });
 }
 
-promptUser();
+promptUser()
+  .then((teamInfo) => {
+    return generatePage(teamArr);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// .then(teamArr => {
-//     return generatePage(teamArr)
-// })
+const writeFile = (fileContent) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/index.html", fileContent, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+};
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: "Stylesheet created!",
+      });
+    });
+  });
+};
--- src/templateHtml.js
+++ src/templateHtml.js
@@ -1,17 +1,66 @@
-const generatePage = (teamData) => {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>${teamData[0].name}</h1>
-    </body>
-    </html>
-    `;
-};
+const generateCards = teamInfo => {
+ 
+  const manager = teamInfo.manager.map(function(emp) {
+      let managerHtml = `
+      <div class="card" style="width: 18rem;">
+          <h2>${emp.name}</h2>
+          <h4>Manager<h4>
+          <p>ID: ${emp.id}</p>
+          <p>Email: <a href="mailto:${emp.email}">${emp.email}</a></p>
+          <p>Office Number: ${emp.officeNumber}</p>
+      </div>
+      `
+      return managerHtml
+  });
 
-module.exports = generatePage
+  const engineer = teamInfo.engineer.map(function(emp) {
+      let engineerHtml = `
+      <div class="card" style="width: 18rem;">
+          <h2>${emp.name}</h2>
+          <h4>Engineer<h4>
+          <p>ID: ${emp.id}</p>
+          <p>Email: <a href="mailto:${emp.email}">${emp.email}</a></p>
+          <p> Github: <a href="https://github.com/${emp.github}" target="_blank">${emp.github}</a></p>
+      </div>
+      `
+      return engineerHtml
+  })
+
+  const intern = teamInfo.intern.map(function(emp) {
+      let interHtml = `
+      <div class="card" style="width: 18rem;">
+          <h2>${emp.name}</h2>
+          <h4>Intern<h4>
+          <p>ID: ${emp.id}</p>
+          <p>Email: <a href="mailto:${emp.email}">${emp.email}</a></p>
+          <p> School: ${emp.school}</p>
+      </div>
+      `
+      return interHtml
+  })
+  return [manager,engineer,intern];
+}
+
+module.exports = templateData => {
+  return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+          <title>My Team</title>
+      </head>
+      <body>
+          <header>
+          <h1 class="text-center">My Team</h1>
+          </header>
+          ${generateCards(templateData)}
+
+
+      </body>
+      </html>    
+      `
+}
+
+
